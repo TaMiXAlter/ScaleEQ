fd92e04b89b4e385f4a1281a619174606a5b4bb9
TaMi <gary321588@gmail.com>
2024-08-18 17:07:54 +0800

    我永遠喜歡廣井菊里

--- Assets/Scripts/Terrain/SplineCreater.cs
+++ Assets/Scripts/Terrain/SplineCreater.cs
@@ -27,14 +27,23 @@ public class SplineCreater : MonoBehaviour
     [SerializeField]
     private EQController eqController;
     
+    [Header("Edge")]
+    [SerializeField]
+    private float TopEdge = 10f;
+    [SerializeField]
+    private float BottomEdge = -10f;
+    
+    [Header("Spline")]
     [SerializeField] 
     private float XKnotStartPoint ;
     [SerializeField]
     private float XSpaceing ;
-    [SerializeField]
-    private float YAduioModifier = 1f ;
-    [SerializeField]
-    private float YControllerModifier=1f ;
+    
+    [Header("Modifier")]
+    [SerializeField][Range(0,1f)]
+    private float YAduioModifier = 0.5f ;
+    [SerializeField][Range(0,1f)]
+    private float YControllerModifier= 0.5f ;
     
     private SpriteShapeController _spriteShapeController;
     Vector3[] _currentPositions = new Vector3 [8];
@@ -45,6 +54,7 @@ public class SplineCreater : MonoBehaviour
     private void Start() {
         InitSpriteSpline();
         if(!eqController) Debug.LogError("eqController is null");
+        UpdateSpriteSpline();
     }
 
     private void Update() {
@@ -64,13 +74,13 @@ public class SplineCreater : MonoBehaviour
     {
         float[] freqBand = AudioManager.Instance.GetAudioBuffer();
         for (int i = 0; i < freqBand.Length; i++) {
-            float Audio = freqBand[i] * YAduioModifier;
-            float EQM = GetEQMultiplyer(i) * YControllerModifier;
-            float UpdatePositionY = Audio + EQM;
+            float Control = freqBand[i] *YAduioModifier + GetEQMultiplyer(i) * YControllerModifier;
+
+            float UpdatePositionY = Mathf.Lerp(BottomEdge,TopEdge, Control);
             
             Vector3 targetPosition = new Vector3(GetKnotXPosition(i), UpdatePositionY, 0);
+            //smoothing
             _currentPositions[i] = Vector3.Lerp(_currentPositions[i], targetPosition, Time.deltaTime);
-        
             _spriteShapeController.spline.SetPosition(i, _currentPositions[i]);
         }
     }
@@ -89,7 +99,7 @@ public class SplineCreater : MonoBehaviour
         }
         
         float G1 = G1Scale  * eqController.controlPointsValue[0];
-        float G2 =  G2Scale * eqController.controlPointsValue[1] ;
+        float G2 = G2Scale * eqController.controlPointsValue[1] ;
         float G3 = G3Scale  * eqController.controlPointsValue[2];
         float value = G1 + G2 + G3 ;
         return value;
