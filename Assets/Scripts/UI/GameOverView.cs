8f04023f78d4bac1abc764c6e98b567bccf0a9b7
Frank92613 <frankyen0613@gmail.com>
2024-08-20 20:20:31 +0800

    121w213

--- Assets/Scripts/UI/GameOverView.cs
+++ Assets/Scripts/UI/GameOverView.cs
@@ -3,15 +3,16 @@ using UnityEngine.UI;
 
 namespace UI
 {
-    public class GameOverView:GamePlayUIView
+    public class GameOverView : GamePlayUIView
     {
-        private Button Restart, Quit;
+
+        Button Restart, Quit;
 
         private void OnEnable()
         {
             Restart = transform.Find("Restart").GetComponent<Button>();
             Quit = transform.Find("Quit").GetComponent<Button>();
-            
+
             Restart.onClick.AddListener(() => { GameManager.Instance.ChangeScene(GameManager.Instance.CurrentSceneName); });
             Quit.onClick.AddListener(() => { GameManager.Instance.ChangeScene("LevelSelect"); });
         }
